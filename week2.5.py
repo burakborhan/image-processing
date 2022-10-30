88f0118bb1a10a97ee2ea9ea8d08e5ea54b80445
burakborhan <108622201+burakborhan@users.noreply.github.com>
2022-10-30 21:32:29 +0300

    week2

---
+++ week2.5.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+C = np.array([[1, 1, 2], [3, 5, 3], [5, 6, 9]])
+s = sum ( C[ : ] )
+print(s)
+a = sum(C)
+print(a)
---
+++ week2.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+resim1=cv2.imread("ben.png")
+cv2.imshow("ben".resim1)
+
+print(resim1.size)
+
+print(resim1.dtype)
+
+print(resim1.shape)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
