29216ee8b0866dbddb16cbc4dee3a4ba1964f6cd
burakborhan <108622201+burakborhan@users.noreply.github.com>
2022-11-13 20:54:18 +0300

    week4

---
+++ Week4.py
@@ -0,0 +1,19 @@
+# importing the modules needed
+import cv2
+import numpy as np
+
+# Reading the image
+image = cv2.imread('res.png')
+
+# Creating the kernel(2d convolution matrix)
+kernel1 = np.ones((5, 5), np.float32)/30
+
+# Applying the filter2D() function
+img = cv2.filter2D(src=image, ddepth=-1, kernel=kernel1)
+
+# Shoeing the original and output image
+cv2.imshow('Original', image)
+cv2.imshow('Kernel Blur', img)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
